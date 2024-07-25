ccb709a91e712c388349348a7b981f84b6e882f4
unknown <hashmiabdulmajid@gmail.com>
2024-07-25 12:16:59 +0530

    some updates

--- controller/productcontroller.js
+++ controller/productcontroller.js
@@ -1,39 +1,57 @@
-const productModel =require('../model/productmodel');
-const createproduct = async (req,res)=>{
-const body = req.body
-  try{
+const productModel = require("../model/productmodel");
+const createproduct = async (req, res) => {
+  const body = req.body;
+  try {
     await productModel.create({
+      name: body.name,
+      price: body.price,
+      mrp: body.mrp,
+      weight: body.weight,
+      image1: body.image1,
+      image2: body.image2,
+      image3: body.image3,
+      image4: body.image4,
+      description: body.description,
+    });
+    return res.status(201).json("entry created");
+  } catch (err) {
+    return res.status(402).json(err);
+  }
+};
 
-    name:body.name,
-    price:body.price,
-    mrp:body.mrp,
-    weight:body.weight,
-    image1:body.image1,
-    image2:body.image2,
-    image3:body.image3,
-    image4:body.image4,
-    description:body.description
-})
-return res.status(201).json("entry created");
-}
-catch(err){
-return res.status(402).json(err);
-}
-
-}
-
-
-const findproduct =async (req,res)=>{
-try{
-    const product=await productModel.find()
-    console.log(product);
-res.status(201).json(product);
-}
-catch(err){
-
+const findproduct = async (req, res) => {
+  try {
+    const product = await productModel.find();
+  
+    res.status(201).json(product);
+  } catch (err) {
     res.status(404).json(err);
-}
+  }
+};
+
+const editproduct = async (req,res) => {
+  const body = req.body;
+
+  try {
+   const updateproduct= await productModel.findByIdAndUpdate(body._id,body);
+        res.send({message:"updated successfully",data:updateproduct,success:true});
+
+  } catch (err) {
+    res.send({message:"internal server error",success:false});
+  }
+};
 
 
+const deleteproduct =async(req,res)=>{
+  const body=req.body;
+  console.log(body);
+
+  try{
+  const deletedproduct = await productModel.findByIdAndDelete(body._id);
+  res.send({message:"product deleted",success:true,data:deletedproduct});
+}catch(err){
+
+  res.send({message:"Internam server error",success:false})
 }
-module.exports = {createproduct,findproduct};
+}
+module.exports = { editproduct, createproduct, findproduct ,deleteproduct};
--- routes/productroute.js
+++ routes/productroute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const productrouter = express.Router();
-const {createproduct,findproduct} =require('../controller/productcontroller.js');
+const {createproduct,findproduct,editproduct, deleteproduct} =require('../controller/productcontroller.js');
 const adminauth = require("../middlewares/adminauth.js");
 
 
 productrouter.post('/',adminauth,createproduct);
-productrouter.get('/',adminauth,findproduct)
+productrouter.get('/',adminauth,findproduct);
+productrouter.put('/edit',adminauth,editproduct);
+productrouter.delete('/delete',adminauth,deleteproduct);
 
 module.exports = productrouter;
